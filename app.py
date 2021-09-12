2eb0275846397a269e52d42e7aec7c34c398be51
Harrison Lanier <60249654+hlanier14@users.noreply.github.com>
2021-09-11 23:07:23 -0500

    Clean up code

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def main():
     LINK_contract_abi = cred["ABI"]["Chainlink"]
     BlockFrostAPI = cred["API"]["BlockFrost"]
     CoinMarketCapAPI = cred["API"]["CoinMarketCap"]
+    EtherscanAPI = cred["API"]["Etherscan"]
     InfuraAPI = cred["API"]["Infura"]
     ETH_wallet_address = cred["Wallet"]["ETH"]
     ADA_wallet_address = cred["Wallet"]["ADA"]
@@ -22,31 +23,16 @@ def main():
     c.close()
 
     # Create wallet objects
-    ETH_wallet = ETHWallet(InfuraAPI, CoinMarketCapAPI, ETH_wallet_address, "ETH", 18, ETH_contract_address)
-    LINK_wallet = ETHWallet(InfuraAPI, CoinMarketCapAPI, ETH_wallet_address, "LINK", 18, LINK_contract_address, LINK_contract_abi)
+    ETH_wallet = ETHWallet(InfuraAPI, CoinMarketCapAPI, EtherscanAPI, ETH_wallet_address, "ETH", 18, ETH_contract_address)
+    LINK_wallet = ETHWallet(InfuraAPI, CoinMarketCapAPI, EtherscanAPI, ETH_wallet_address, "LINK", 18, LINK_contract_address, LINK_contract_abi)
     ADA_wallet = ADAWallet(BlockFrostAPI, CoinMarketCapAPI, ADA_wallet_address, "ADA", 6)
     DOT_wallet = DOTWallet(CoinMarketCapAPI, DOT_wallet_address, "DOT", 10)
 
     portfolio = Portfolio([ETH_wallet, LINK_wallet, ADA_wallet, DOT_wallet], "data\positions.csv")
     #portfolio.update_positions()
-
-    positions = portfolio.get_positions()
-
-    total_value = sum(positions["Value"])
-    positions["% of Total"] = positions["Value"] / total_value
-    positions = positions.sort_values(by="Value")
-
-    print(total_value)
-    print(positions)
+    #positions = portfolio.get_positions()
 
     # Get transactions
-
-"""
-    print("\n ETH balance: {} \n LINK balance: {} \n ADA balance: {} \n DOT balance: {} \n".format(ETH_wallet.get_balance(),
-                                                                                                LINK_wallet.get_balance(),
-                                                                                                ADA_wallet.get_balance(),
-                                                                                                DOT_wallet.get_balance()))
-"""
 
 
 if __name__ == "__main__":
