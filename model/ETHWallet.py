541b32658484d89c1f670abd030906cc433b756b
Harrison Lanier <60249654+hlanier14@users.noreply.github.com>
2021-09-11 23:08:56 -0500

    Clean up code

--- model/ETHWallet.py
+++ model/ETHWallet.py
@@ -4,35 +4,28 @@ import requests
 
 class ETHWallet(Wallet):
 
-    ETH_address = "0x0000000000000000000000000000000000000000"
-
-    def __init__(self, _infuraAPI, _priceAPI, _wallet_address, _asset_symbol, _asset_decimals, _asset_contract_address, _asset_contract_abi=None):
-        self.web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/{}'.format(_infuraAPI)))
+    def __init__(self, _scannerAPI, _priceAPI, _transactionAPI, _wallet_address, _asset_symbol, _asset_decimals, _asset_contract_address, _asset_contract_abi=None):
+        
+        self.web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/{}'.format(_scannerAPI)))
         self.priceAPI = _priceAPI
-        self.isToken = False
+        self.transactionAPI = _transactionAPI
+
         self.asset_symbol = _asset_symbol
         self.asset_decimals = _asset_decimals
-        self.wallet_address = _wallet_address
-        # if the asset is not ETH, instantiate ETH token with contract address and abi
-        if _asset_contract_address != self.ETH_address:
+        self.isToken = False
+        if _asset_contract_address != "0x0000000000000000000000000000000000000000":
             self.token = self.web3.eth.contract(address=_asset_contract_address, abi=_asset_contract_abi)
             self.isToken = True
+            
+        self.wallet_address = _wallet_address
     
     def get_balance(self):
         # Return balance of ETH or token using Web3
         if self.isToken:
-            return self.token.functions.balanceOf(self.wallet_address).call() / 10**self.asset_decimals
-        return self.web3.eth.getBalance(self.wallet_address) / 10**self.asset_decimals
+            return self.token.functions.balanceOf(self.wallet_address).call() / 10 ** self.asset_decimals
+        return self.web3.eth.getBalance(self.wallet_address) / 10 ** self.asset_decimals
 
-    def get_asset_price(self):
-        # Return asset price from CoinMarketCap
-        price = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", 
-                             headers={'X-CMC_PRO_API_KEY': self.priceAPI}, 
-                             params={"symbol":self.asset_symbol}).json()
-        return float(price["data"][self.asset_symbol]["quote"]["USD"]["price"])
-
-    def get_wallet_value(self):
-        return self.get_asset_price() * self.get_balance()
-
-    def get_asset_symbol(self):
-        return self.asset_symbol
+    def get_transactions(self):
+        if self.isToken:
+            return  requests.get("https://api.etherscan.io/api?module=account&action=tokentx&address={}&startblock=0&endblock=999999999&sort=asc&apikey={}".format(self.wallet_address, self.transactionAPI)).json()
+        return requests.get("https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&sort=asc&apikey={}".format(self.wallet_address, self.transactionAPI)).json()
